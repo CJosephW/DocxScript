248718bb4a457d1d4de58fcf57d0387d4a26e82b
Cameron Wise <wisecameron1@gmail.com>
2020-01-08 13:22:00 +0000

    Posts skill details to /skills route now

--- docx_testing.py
+++ docx_testing.py
@@ -1,5 +1,11 @@
 from docx import Document
 from docx import table
+from pymongo import MongoClient
+from pprint import pprint
+from collections import defaultdict
+import requests
+
+database = "http://localhost:3000/v1/skills"
 
 
 document = Document('9781284151398_SESx_CH37.12.docx')
@@ -12,7 +18,24 @@ taskLocation = (2, 0)
 performanceOutcomeLocation = (3, 0)
 candidateDirectivesLocation = (4, 0)
 
-#GET TASKS
+taskList = []
+
+currentPost = {
+    'title': table.cell(*titleLocation).text,
+    'evaluator_instructions': table.cell(*evaluatorInstructionsLocation).text,
+    'candidate_driectives': table.cell(*candidateDirectivesLocation).text,
+}
+docInfo = [table.cell(*titleLocation).text, table.cell(*evaluatorInstructionsLocation).text,
+    table.cell(*taskLocation).text, table.cell(*performanceOutcomeLocation).text, table.cell(*candidateDirectivesLocation).text]
+
 for row in range(7, len(table.rows) - 1): # get tasks remove last row
-    print(table.cell(row, 1).text)
+    print(table.cell(row, 1).text)    
+    taskList.append(table.cell(row, 1).text)
+
+currentPost['tasks'] = taskList#add tasks in list format to dictionary with the key 'tasks'
+
 print(table.cell(*taskLocation).text)
+
+r = requests.post(url = database, json = currentPost)
+
+
