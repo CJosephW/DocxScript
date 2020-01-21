ffef94d484a34174599a10c6c7e2197e118a6f1c
Cameron Wise <wisecameron1@gmail.com>
2020-01-21 13:45:45 +0000

    POST not working when enumerating through folder

--- docx_testing.py
+++ docx_testing.py
@@ -4,38 +4,46 @@ from pymongo import MongoClient
 from pprint import pprint
 from collections import defaultdict
 import requests
-
+import os
+from pathlib import Path
+import time
+directory = os.getcwd()
 database = "http://localhost:3000/v1/skills"
 
+for filename in os.listdir(directory):
+    if filename.endswith('.docx'):
 
-document = Document('9781284151398_SESx_CH37.12.docx')
-table = document.tables[0]
+        document = Document(Path(filename).absolute())
 
-# GET HEADER INFORMATION (Title, task, task #, etc)
-titleLocation = (0, 0)
-evaluatorInstructionsLocation = (1, 0)
-taskLocation = (2, 0)
-performanceOutcomeLocation = (3, 0)
-candidateDirectivesLocation = (4, 0)
+        table = document.tables[0]
 
-taskList = []
+        # GET HEADER INFORMATION (Title, task, task #, etc)
+        titleLocation = (0, 0)
+        evaluatorInstructionsLocation = (1, 0)
+        taskLocation = (2, 0)
+        performanceOutcomeLocation = (3, 0)
+        candidateDirectivesLocation = (4, 0)
 
-currentPost = {
-    'title': table.cell(*titleLocation).text,
-    'evaluator_instructions': table.cell(*evaluatorInstructionsLocation).text,
-    'candidate_driectives': table.cell(*candidateDirectivesLocation).text,
-}
-docInfo = [table.cell(*titleLocation).text, table.cell(*evaluatorInstructionsLocation).text,
-    table.cell(*taskLocation).text, table.cell(*performanceOutcomeLocation).text, table.cell(*candidateDirectivesLocation).text]
+        taskList = []
 
-for row in range(7, len(table.rows) - 1): # get tasks remove last row
-    print(table.cell(row, 1).text)    
-    taskList.append(table.cell(row, 1).text)
+        currentPost = {
+            'title': table.cell(*titleLocation).text,
+            'evaluator_instructions': table.cell(*evaluatorInstructionsLocation).text,
+            'candidate_driectives': table.cell(*candidateDirectivesLocation).text,
+        }
+        docInfo = [table.cell(*titleLocation).text, table.cell(*evaluatorInstructionsLocation).text,
+            table.cell(*taskLocation).text, table.cell(*performanceOutcomeLocation).text, table.cell(*candidateDirectivesLocation).text]
 
-currentPost['tasks'] = taskList#add tasks in list format to dictionary with the key 'tasks'
+        for row in range(7, len(table.rows) - 1): # get tasks remove last row
+            print(table.cell(row, 1).text)    
+            taskList.append(table.cell(row, 1).text)
 
-print(table.cell(*taskLocation).text)
+        currentPost['tasks'] = taskList#add tasks in list format to dictionary with the key 'tasks'
 
-r = requests.post(url = database, json = currentPost)
+        print(table.cell(*taskLocation).text)
 
+        r = requests.post(url = database, json = currentPost)
 
+        
+    else:
+        continue
